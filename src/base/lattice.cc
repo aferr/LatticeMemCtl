99cdeec0b898e4647ff35583b7c088d6d69b9677
Andrew Ferraiuolo <andrew@csl.cornell.edu>
2015-03-02 17:16:42 -0500

    Added more lattice stuff. Untested

--- src/base/lattice.cc
+++ src/base/lattice.cc
@@ -1,22 +1,52 @@
 #include "lattice.hh"
 Lattice* Lattice::lattice = 0;
 
-SecurityClass::SecurityClass(int _val) : val(_val){}
+SecurityClass::SecurityClass(int _val) : val(_val){
+}
 
 bool SecurityClass::operator<=(SecurityClass* that){
-    return this->val <= that->val;
+  if(this == that) return true;
+  return Lattice::instance()->has_rule(this,that);
+}
+
+std::vector<SecurityClass*>::iterator
+SecurityClass::upper_list(){
+  if(!upper_list_){
+    std::vector<SecurityClass*>* upper_list_ = new std::vector<SecurityClass*>();
+    Lattice::iterator it = Lattice::instance()->begin();
+    for(; it!=Lattice::instance()->end(); it++){
+      if(this <= it->second) upper_list_->push_back(it->second);
+    }
+  }
+  return upper_list_->begin();
+}
+
+SecurityClass* SecurityClass::next_upper(){
+  if(is_top()) return this;
+  if(*ul_circular == NULL) ul_circular = upper_list();
+  if(ul_circular == upper_list_->end()){
+    ul_circular = upper_list();
+  } else {
+    ul_circular = ul_circular++;
+  }
+  return *ul_circular;
+}
+
+bool SecurityClass::is_top(){
+  return (upper_list()++) == upper_list_->end();
 }
 
 SecurityClass* SecurityClass::join(SecurityClass* that){
+    // TODO this is not correct
     if(this->val <= that->val) return that;
     return this;
 }
 
 SecurityClass* SecurityClass::meet(SecurityClass* that){
+     // TODO this is not correct
     if(this->val <= that->val) return this;
     return that;
 }
-
 
 Lattice::Lattice(){
     for(int i=0; i<8; i++){
@@ -24,10 +54,15 @@ Lattice::Lattice(){
     }
 }
 
-
 Lattice* Lattice::instance(){
     if(!lattice){
         lattice = new Lattice();
     }
     return lattice;
 }
+
+bool Lattice::has_rule(SecurityClass* x, SecurityClass* y){
+  return Lattice::instance()->rules[x].find(y) !=
+    Lattice::instance()->rules[x].end();
+}
+
--- src/base/lattice.hh
+++ src/base/lattice.hh
@@ -1,25 +1,33 @@
 #include <map>
+#include <iterator>
+#include <vector>
+#include <set>
 
+class Lattice;
 
-class SecurityClass{
+class SecurityClass {
     public:
     SecurityClass(int _val);
     bool operator<=(SecurityClass* that);
     SecurityClass* join(SecurityClass* that);
     SecurityClass* meet(SecurityClass* that);
+    std::vector<SecurityClass*>::iterator upper_list();
+    SecurityClass* next_upper();
+    bool is_top();
 
     private:
     int val;
+    std::vector<SecurityClass*>* upper_list_;
+    std::vector<SecurityClass*>::iterator ul_circular;
 };
 
 class Lattice : public std::map<int,SecurityClass*>{
+    public:
+    static Lattice* instance();
+    static bool has_rule(SecurityClass*, SecurityClass*);
 
     private:
+    map<SecurityClass*,std::set<SecurityClass*> > rules;
     static Lattice* lattice;
-
     Lattice();
-    
-    static Lattice* instance();
-
 };
-
