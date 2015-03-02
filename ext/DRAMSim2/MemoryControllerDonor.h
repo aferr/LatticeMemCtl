0d14f81df7b81828daf412178a6177ad3ebaf143
Andrew <andrew@csl.cornell.edu>
2015-03-01 20:05:15 -0500

    Add new memory controllers

---
+++ ext/DRAMSim2/MemoryControllerDonor.h
@@ -0,0 +1,21 @@
+#include "MemoryControllerMonotonic.h"
+
+using namespace std;
+
+namespace DRAMSim
+{
+    class MemoryControllerDonor : public MemoryControllerTP
+    {
+        public:
+            MemoryControllerDonor(MemorySystem* ms, CSVWriter &csvOut_, 
+                    ostream &dramsim_log_, 
+                    const string &outputFilename_,
+                    unsigned tpTurnLength_,
+                    bool genTrace_,
+                    const string &traceFilename_,
+                    int num_pids_, bool fixAddr,
+                    bool diffPeriod, int p0Period, int p1Period,
+                    int offset);
+
+    };
+}
